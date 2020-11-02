4842883907283e17088b555a72a42d2245e2189e
moa-mel <52823454+moa-mel@users.noreply.github.com>
2020-11-02 02:06:37 +0100

    Updates

---
+++ precache-manifest.8d593c240a4eba8e069415fe0ae9d134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6354466739f841279a7cf4028bba0f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8fef553a6a11b412801",
+    "url": "/static/css/main.f5d6cf12.chunk.css"
+  },
+  {
+    "revision": "21b9aa93c9af0b861be6",
+    "url": "/static/js/2.3b1b0697.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3b1b0697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fef553a6a11b412801",
+    "url": "/static/js/main.44fb06f9.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
